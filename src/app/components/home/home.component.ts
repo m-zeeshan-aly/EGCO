862f4aedd3e10f3c7d3a41826f55f78740faf4ce
abdul-ahad <git@github.com:mughirarehman2573/office-management-system.git>
2024-12-07 20:15:34 +0500

    Added a About US Component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ContactComponent } from '../contact/contact/contact.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,ContactComponent],
+  imports: [NavbarComponent,FooterComponent,ContactComponent,AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
