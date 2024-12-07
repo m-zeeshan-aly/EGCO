d3e4dbbbc4c8e32188a5123797ab170ea4a37616
m-zeeshan-aly <m.zeeshan.ali0346@gmail.com>
2024-12-07 17:24:38 +0500

    Prcocess styling added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,11 +3,17 @@ import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { ContactComponent } from '../contact/contact/contact.component';
 import { ServicesComponent } from '../services/services.component';
+import { ProcessComponent } from '../process/process.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,ContactComponent,ServicesComponent],
+  imports: [NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+    ServicesComponent,
+    ProcessComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
